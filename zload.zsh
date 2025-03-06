75242907e4bc16845ec606cd9c00a2ccfe076305
ttii6 <testwe@126.com>
2025-03-06 09:32:50 -0800

    Update zload.zsh

--- zload.zsh
+++ zload.zsh
@@ -194,7 +194,7 @@ load() {
     done
     
     # Compile the script and .zcompdump only if they have changed
-    my_zcompile "$(realpath "$0")"
+    my_zcompile "$ZLOAD_HOME/bin/zload.zsh" 
     my_zcompile "$comp_dumpfile"
 }
 
